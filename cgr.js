dab6774b7b055cef66cbd398659764ca48bd8735
almeida jonas <almeida.jonas@gmail.com>
2011-04-04 13:16:05 -0500

    aphabet extraction started

--- cgr.js
+++ cgr.js
@@ -1,1 +1,15 @@
 console.log('CGR')
+
+usm = function (seq,abc){ // Universal Sequence Map
+	this.seq={};
+	this.seq.alphabet=function(seqString){//extracts alphabet
+		if (!seqString){seqString=this.string} //uses own string if argument not provided
+		return seqString;
+	}
+
+	if (seq){// if the sequence was submitted
+		this.seq.string=seq;
+		if (!abc){this.seq.abc=this.seq.alphabet()}// but the alphabet was not provided		
+		else {this.seq.abc=abc}
+	}
+}
