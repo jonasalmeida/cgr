c37d2345ad408a8cef037bda1cb7af2b0de0e5f2
almeida jonas <almeida.jonas@gmail.com>
2011-04-15 12:00:19 -0500

    compact and sparce mapping

--- cgr.js
+++ cgr.js
@@ -2,8 +2,8 @@
 
 console.log('CGR');
 
-// Overload types
-"".__proto__.toArray = function(){ // creates an Array with each character of teh string as an element
+// Overload String type, not sure it is a good idea, could interfere with other overloads
+String.prototype.toArray = function(){ // creates an Array with each character of teh string as an element
 	var y=[];
 	for (var i=0;i<this.length;i++){
 		y[i]=this[i];
@@ -11,25 +11,49 @@ console.log('CGR');
 	return y;
 }
 
-"".__proto__.sort=function(){ // sort characters of a string
+String.prototype.sort=function(){ // sort characters of a string
 	var y =this.toArray().sort().toString().replace(/,/g,'');
 	return y;
 }
 
-
 usm = function (seq,abc){ // Universal Sequence Map
-	this.seq={};
-	this.seq.alphabet=function(seqString){//extracts alphabet
-		if (!seqString){seqString=this.string} //uses own string if argument not provided
+	this.alphabet=function(seqString){//extracts alphabet
+		if (!seqString){seqString=this.str} //uses own string if argument not provided
 		var abc='';
 		for (var i=0;i<seqString.length;i++){
 			if (!abc.match(new RegExp(seqString[i]))){abc+=seqString[i]}
 		}
 		return abc.sort(); // using overloaded String.sort()
 	}
-	if (seq){// if the sequence was submitted
-		this.seq.string=seq;
-		if (!abc){this.seq.abc=this.seq.alphabet()}// but the alphabet was not included		
-		else {this.seq.abc=abc}
+	
+	if (seq){this.str=seq}
+	if (!abc){this.abc=this.alphabet()}// extract alphabet		
+	else {this.abc=abc}
+	
+	var m = this.abc.length;var n = this.str.length;
+	this.str2bin = function(map){
+		if (!map){map='sparce'}
+		this.bin=[];
+		switch (map){
+		case 'sparce':
+			for (var j=0;j<m;j++){
+				this.bin[j]=[];
+				for (i=0;i<n;i++){
+					if (this.str[i]===this.abc[j]){this.bin[j][i]=1}
+					else {this.bin[j][i]=0}
+				}
+			}
+			break;
+		case 'compact':
+			for (var j=0;j<Math.ceil(Math.log(8)/Math.log(2));j++){
+				this.bin[j]=[];
+			}
+
+
+		}
+		
+		//console.log('Size: [',n,',',m,']');
 	}
+
+	this.str2bin();
 }
